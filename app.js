ff3f7c27808c47cb60db4c65260d427e67cf1e72
soumyajit4419 <ashish454570@gmail.com>
2019-09-29 22:21:03 +0530

    added frontend

---
+++ app.js
@@ -0,0 +1,38 @@
+var t;
+var x;
+$(".val1").click(function () {
+     x = $("#budget").val();
+    if (x == '') { alert("YOU MUST ENTER SOMETHING"); }
+    else {
+        console.log(x);
+    }
+    $(".d1").text(x);
+    $(".d2").text(x);
+});
+
+$(".val2").click(function () {
+    t = $("#item").val();
+    if (t == '') { alert("YOU MUST ENTER SOMETHING"); }
+    else {
+        console.log(t);
+    }
+});
+
+$(".val3").click(function () {
+    var y = $("#spent").val();
+    if (y == '') { alert("YOU MUST ENTER SOMETHING"); }
+    else {
+        console.log(y);
+        add(t, y);
+      
+
+    }
+});
+
+function add(t,y)
+{
+   $("#main").append(`<li class="list-group-item d-flex justify-content-between align-items-center">
+   ${t}
+   <span class="badge badge-primary badge-pill">${y}</span>
+</li>`)
+}
